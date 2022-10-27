13846eb4e455097b52705d616a3b29a399bde336
IlinSergey <Ilin_sergey90@mail.ru>
2022-10-27 07:55:53 +0300

    ---

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+def binary_search(list, item):
+    low = 0
+    hight = len(list) - 1
+
+    while low <= hight:
+        mid = (low + hight) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        elif guess > item:
+            hight = mid -1
+        else:
+            low = mid + 1
+    return None
+
+item_list = [0, 11, 2, 5, 8 ,7, 11, -1]
+
+print(binary_search(item_list, 2))
+
 
--- tests/test_binary_search_func.py
+++ tests/test_binary_search_func.py
@@ -1,1 +1,8 @@
-import 
+from main import binary_search
+import pytest
+
+@pytest.mark.parametrize('list, item, expected_result', [([0, 1, 2, 5, 8 ,9, 11, 15], 5, 3),
+                                                         ([2, 4, 6, 15, 24, 32, 44], 44, 6),
+                                                         ([0, 2, 3, 5, 11, 12, 24], 4, None)])
+def test_binary_search(list, item, expected_result):
+    assert binary_search(list, item) == expected_result
